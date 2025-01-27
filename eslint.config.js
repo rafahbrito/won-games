7664355b09322a9ff9850bd5240af6b90a4f9bd6
Rafael Brito <henrique8264@gmail.com>
2025-01-27 19:40:25 -0300

    chore: :heavy_plus_sign: Adiciona configuração ESLint e Prettier ao projeto.

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,9 +18,12 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...eslintPluginPrettier.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },

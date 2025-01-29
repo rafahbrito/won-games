7959ca338058430ecfb299fa78499ffc171cbfd5
Rafael Brito <henrique8264@gmail.com>
2025-01-28 23:33:06 -0300

    chore: :hammer: Adicionar script de testes ao git hook

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,7 +4,8 @@ const buildCommand = (filenames) => {
   const files = filenames.map((file) => path.relative(process.cwd(), file))
   return [
     `npx prettier --write ${files.join(' --file')}`,
-    `npx eslint --fix ${files.join(' ')}`
+    `npx eslint --fix ${files.join(' ')}`,
+    `npm test -- --findRelatedTests ${files.join(' ')}`
   ]
 }
 
